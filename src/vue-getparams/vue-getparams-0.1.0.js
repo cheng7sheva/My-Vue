5305f9b8e024e2a42fd1f07bf3dceb476fd3af71
cheng7sheva <38774909+cheng7sheva@users.noreply.github.com>
2019-07-11 16:10:08 +0800

    完成第一版

--- src/vue-getparams/vue-getparams-0.1.0.js
+++ src/vue-getparams/vue-getparams-0.1.0.js
@@ -2,17 +2,18 @@ var VueGPFURL={}
 VueGPFURL.install=function (Vue) {
   Vue.mixin({
     created: function () {
+      this.$_GET={};
       if(!this.$route) return false;
       //FIXME $route的match暂时没发现如何会出现多个匹配，如果出问题随时修改
       var m = this.$route.path.match(this.$route.matched[0].regex);
-      console.log(m);
-      // var params=m[1];
-      // params=params.split("/");
-      // var __get={};
-      // for(var i=0;i<params.length;i+=2){
-      //   __get[params[i]]=decodeURIComponent(params[i+1]);
-      // }
-      // this.$_GET=__get;
+      if(!m[1]) return false;
+      var params=m[1];
+      params=params.split("/");
+      var __get={};
+      for(var i=0;i<params.length;i+=2){
+        __get[params[i]]=decodeURIComponent(params[i+1]);
+      }
+      this.$_GET=__get;
     }
   });
 };
